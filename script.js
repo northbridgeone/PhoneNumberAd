4591b2f681188c02d1dd144971a040a87ec1040b
æx <6d6b2f61796363@gmail.com>
2023-07-24 03:36:36 -0500

    001

---
+++ script.js
@@ -0,0 +1,32 @@
+var canvas = document.getElementById('staticBackground');
+var context = canvas.getContext('2d');
+
+// Size the canvas to the window
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Update canvas size when window is resized
+window.onresize = function() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    var phoneNumberDiv = document.getElementById("phoneNumber");
+    phoneNumberDiv.style.top = window.innerHeight / 2 + "px";
+    phoneNumberDiv.style.left = window.innerWidth / 2 + "px";
+};
+
+// Create static effect
+function renderStatic() {
+    var imageData = context.createImageData(canvas.width, canvas.height);
+    var data = imageData.data;
+    for (var i = 0; i < data.length; i += 4) {
+        var color = Math.random() * 255;
+        data[i] = color;     // red
+        data[i + 1] = color; // green
+        data[i + 2] = color; // blue
+        data[i + 3] = 255;   // alpha
+    }
+    context.putImageData(imageData, 0, 0);
+}
+
+// Re-render static every 100ms
+setInterval(renderStatic, 100);
